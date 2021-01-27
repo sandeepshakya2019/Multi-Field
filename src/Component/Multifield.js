fe94d2a1ea532eee3e2e73afe69298a107af00fc
sandeepshakya2019 <sandeepshakya2015@gmail.com>
2021-01-27 14:58:48 +0530

    first commit

---
+++ src/Component/Multifield.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import ReactFileReader from "react-file-reader";
+// import { CsvToHtmlTable } from "react-csv-to-table";
+// import { CSVReader } from "react-papaparse";
+import { readString } from "react-papaparse";
+import "./M.css";
+
+function Multifield() {
+  const [csvdata, setCsvdata] = useState("");
+  //   const [csvdata2, setCsvdata2] = useState("");
+
+  const handleFiles = (files) => {
+    // files = files.base64;
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      const results = readString(reader.result).data;
+      // var results = readString.parse(reader.result);
+      // console.log(results[0]);
+      let m = [];
+      for (let i = 0; i < 50; i++) {
+        m.push(results[i]);
+      }
+      // console.log(m[1][0]);
+      setCsvdata(m);
+    };
+    reader.readAsText(files[0]);
+  };
+
+  return (
+    <div>
+      <h1>Multi-Field</h1>
+      <div>
+        <ReactFileReader handleFiles={handleFiles} fileTypes={".csv"}>
+          <button className="btn">Upload</button>
+        </ReactFileReader>
+      </div>
+      {csvdata ? (
+        <table className="table table-striped">
+          {csvdata.map((e) => (
+            <tbody className="tbody">
+              <th className="t" scope="col">
+                {e[0]}
+              </th>
+              <th className="t" scope="col">
+                {e[1]}
+              </th>
+              <th className="t" scope="col">
+                {e[2]}
+              </th>
+              <th className="t" scope="col">
+                {e[3]}
+              </th>
+              <th className="t" scope="col">
+                {e[4]}
+              </th>
+            </tbody>
+          ))}
+        </table>
+      ) : null}
+    </div>
+  );
+}
+
+export default Multifield;
